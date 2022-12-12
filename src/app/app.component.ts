ca1b7dfeb7059e97bbad10c25918945802642832
Julian Jones <julianjones@julians-mbp.lan>
2022-12-12 07:00:11 -0500

    reinstall npm to update to v15 to fix issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
   tripData$: any;
   constructor( private TripDataService: TripDataService) { }
   tripList:{entryNum:String,Title:String,Price:String, Date:String, Link:String,Rating:String}[] = trips
-  onProductCreate(nums: {entryNum: string}){
+  onSubmit(nums: {entryNum: string}){
 	console.log(nums)
   }	
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [ AppComponent, HomeComponent ],
-  imports: [ BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [ BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
